2681e821ab45db7442d277947288482eaad38598
leo.quan <leo.quan@leo-quan.corp.vipshop.com>
2017-02-17 19:01:14 +0800

    comm

---
+++ demos/src/main/java/disruptor/MainTest.java
@@ -0,0 +1,15 @@
+package disruptor;
+
+public class MainTest {
+
+    /**
+     * native memory
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 如果cpu为4核，4个线程运行，如果有一个线程运行无阻塞时，那么是否一定会导致CPU被这个线程完全占用？
+        // ------ CPU 线程调度 ----机制
+    }
+
+}
