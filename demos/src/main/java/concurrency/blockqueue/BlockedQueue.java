ca848fce35d9772c487150f8e6d72f0edc241cb1
leo.quan <leo.quan@leo-quan.corp.vipshop.com>
2017-03-16 20:20:50 +0800

    comm

--- demos/src/main/java/concurrency/blockqueue/BlockedQueue.java
+++ demos/src/main/java/concurrency/blockqueue/BlockedQueue.java
@@ -41,20 +41,6 @@ public class BlockedQueue<E> {
         this.jok.equals(null);
 	}
 	
-	{
-	int i = "aaa".length();
-	char ch = 17;
-	int j=012;
-	int l = 023;
-	
-	int  k=0x78;
-	short s =1;
-	s=s+1;//java 会自动向长字节的类型扩展转型
-	short s2=1;
-	s2+=1;//这里的1是short ,很奇怪，也就是说在+，-，*,/的时候，整型数字默认是int
-	
-	}
-	
 
 }
 
