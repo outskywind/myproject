932bde74d3f35de28c5a4aa0a0233ad59cf3bb0f
全承运[产品技术中心] <leo.quan@corp.vipshop.com>
2017-03-29 11:06:21 +0800

    maintest

--- demos/src/main/java/disruptor/MainTest.java
+++ demos/src/main/java/disruptor/MainTest.java
@@ -2,14 +2,28 @@ package disruptor;
 
 public class MainTest {
 
-    /**
-     * native memory
-     * 
-     * @param args
-     */
+
     public static void main(String[] args) {
         // 如果cpu为4核，4个线程运行，如果有一个线程运行无阻塞时，那么是否一定会导致CPU被这个线程完全占用？
         // ------ CPU 线程调度 ----机制
+        for (int i = 0; i < 3; i++) {
+            Thread t = new Thread(new RunNothing());
+            t.start();
+        }
+
+    }
+
+    public static class RunNothing implements Runnable {
+
+        @Override
+        public void run() {
+            int i = 0;
+            while (true) {
+                i++;
+            }
+        }
+
+
     }
 
 }
