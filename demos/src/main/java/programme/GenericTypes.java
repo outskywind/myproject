3edaea9bbfb3c21044ea4aa555e1de54c3b67401
quanchengyun <quanchengyun@dafy.com>
2018-05-07 11:38:55 +0800

    commit

---
+++ demos/src/main/java/programme/GenericTypes.java
@@ -0,0 +1,31 @@
+package programme;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * Created by quanchengyun on 2018/5/3.
+ */
+public class GenericTypes {
+
+
+    /**
+     *  type parameter 指的是静态定义时的那个参数名字 也就是 "T" ,
+     // parameterized type 指的是实例化时动态具体传入的类型
+     */
+    public void test() throws Exception{
+        Method processMethod = this.getClass().getMethod("xxx");
+        Parameter[] params = processMethod.getParameters();
+        ParameterizedType pt = ((ParameterizedType)params[0].getParameterizedType());
+        Class ptc;
+        if(pt==null|| pt.getActualTypeArguments()==null || pt.getActualTypeArguments().length==0){
+            ptc = String.class;
+        }else {
+            ptc = (Class)pt.getActualTypeArguments()[0];
+        }
+    }
+
+
+
+}
